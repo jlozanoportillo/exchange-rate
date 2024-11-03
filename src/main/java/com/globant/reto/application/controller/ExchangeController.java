6924f108e608dce79e500af612cb73c200afc938
josecarlos.lozano <josecarlos.lozano@globant.com>
2024-11-03 11:01:32 -0500

    firma inicial del servicio

--- src/main/java/com/globant/reto/application/controller/ExchangeController.java
+++ src/main/java/com/globant/reto/application/controller/ExchangeController.java
@@ -1,16 +1,30 @@
 package com.globant.reto.application.controller;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.reactivex.Single;
+import reactor.adapter.rxjava.RxJava2Adapter;
+import reactor.core.publisher.Mono;
 
 @RestController()
 @RequestMapping("exchange")
 public class ExchangeController {
-  
+
   @GetMapping(value = "/health")
   public String sayHello() {
     return "I am Live";
   }
-  
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Mono<ResponseEntity<String>> getExchangeMessage() {
+    Single<String> singleMessage = Single.just("Hello from RxJava2!");
+    return RxJava2Adapter.singleToMono(singleMessage).map(ResponseEntity::ok);
+  }
+
 }
