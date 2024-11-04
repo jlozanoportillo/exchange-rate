e77434b12e34325324b5f78c02bcbebf76b2b1c0
Jose Lozano <jlozanoportillo@gmail.com>
2024-11-04 13:52:42 -0500

    borrado de pruebas para corregir

--- src/test/java/com/globant/reto/ExchangeRateApplicationTests.java
+++ src/test/java/com/globant/reto/ExchangeRateApplicationTests.java
@@ -1,13 +1,13 @@
-package com.globant.reto;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ExchangeRateApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.globant.reto;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ExchangeRateApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
